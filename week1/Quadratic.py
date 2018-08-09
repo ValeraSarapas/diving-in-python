70e921ddb3175ce73a0518b5de7905803e08576e
Valera <valera_by@yahoo.com>
2018-08-10 02:09:23 +0300

    fix bug

--- week1/Quadratic.py
+++ week1/Quadratic.py
@@ -16,14 +16,19 @@ if __name__ == "__main__":
         print ("a: {}, b: {}, c: {}".format(a,b,c))
         D= b**2-4*a*c
         print("Дискриминант: ", D)
+        print("D^1/2: ", D**(1/2))
+        print("2a: ", 2*a)
         if D >0:
-            x1 = int((-b +D**1/2) /(2*a))
-            x2 = int((-b -D**1/2) /(2*a))
+            x1 = int((-b +D**(1/2)) /(2*a))
+            x2 = int((-b -D**(1/2)) /(2*a))
             print(f'{x1:d}\n{x2:d}')
         elif D == 0:
             x1=x2= int(-b /(2*a))
             print(f'{x1:d}\n{x2:d}')
         else:
             print("корней на множестве действительных чисел нет")        
+    print("test1: ", a*x1**2+b*x1+c)
+    print("test2: ", a*x2**2+b*x2+c)
+    
         
 
