a1f94883c8897f4c60ba638c5a5df01a482c8379
Valera <valera_by@yahoo.com>
2018-08-16 01:50:44 +0300

    task 2

---
+++ week2/to_json.py
@@ -0,0 +1,16 @@
+import functools
+import json
+def to_json(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs ):
+        result = func(*args, **kwargs)
+        return json.dumps(result)
+    return wrapped
+  
+@to_json
+def get_data():
+  return {
+    'data': 42
+  }
+  
+get_data()  # вернёт '{"data": 42}'
